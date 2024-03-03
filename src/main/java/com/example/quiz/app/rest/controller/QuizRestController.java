f3b609d9569af2094833f2bfc4f692098b595d7e
Alkaria Mandal <alkaria0@gmail.com>
2024-03-03 20:38:04 +0530

    yml file and some logs file added

--- src/main/java/com/example/quiz/app/rest/controller/QuizRestController.java
+++ src/main/java/com/example/quiz/app/rest/controller/QuizRestController.java
@@ -1,5 +1,7 @@
 package com.example.quiz.app.rest.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,11 +18,16 @@ import com.example.quiz.app.service.QuizService;
 @CrossOrigin(origins = "*")
 public class QuizRestController {
 
+	
+	private static final Logger log = LoggerFactory.getLogger(QuizRestController.class);
+
+	
 	@Autowired
 	QuizService quizService;
 	
 	@GetMapping("/get-quiz-data")
 	public ResponseEntity<?> getByTypeCategoryAndDifficulty(@RequestParam("type") String type,@RequestParam("category") String category,@RequestParam("difficulty") String difficulty,@RequestParam("subCategory") String subCategory,@RequestParam("limit") String limit){
+		log.info("REST CONTROLLER :: FETCHING QUIZ DATA RESPONSE FOR "+subCategory);
 		return new ResponseEntity(quizService.getByTypeCategoryAndDifficulty(type,category,difficulty,subCategory,limit),HttpStatus.OK);
 	}
 }
--- src/main/java/com/example/quiz/app/service/QuizService.java
+++ src/main/java/com/example/quiz/app/service/QuizService.java
@@ -4,6 +4,8 @@ import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,10 +15,15 @@ import com.example.quiz.app.repository.QuestionsRepository;
 @Service("quizService")
 public class QuizService {
 	
+	
+	private static final Logger log = LoggerFactory.getLogger(QuizService.class);
+
+	
 	@Autowired
 	QuestionsRepository questionsRepository;
 	
 	public List<Question> getByTypeCategoryAndDifficulty(String type, String category, String difficulty,String subCategory,String limit){
+		log.info("SERVICE :: FETCHING QUIZ DATA RESPONSE FOR "+subCategory);
 		List<Question> getQuestions = questionsRepository.findByTypeCategoryAndDifficulty(type, category, difficulty,subCategory)
 				.stream().limit(Integer.parseInt(limit)).collect(Collectors.toList());
 		return getQuestions;
